5634b98b8c1c88530953789bd6a270f22529c4dd
balmant <tlmaior@gmail.com>
2024-07-31 06:26:46 -0300

    Created the test related to [Backend-Serverless-Database]

--- src/cnc/tests/test_provisioner.py
+++ src/cnc/tests/test_provisioner.py
@@ -342,7 +342,7 @@ class AWSProvisionStageOneServiceServerlessAndTwoResourceDynamoDBTest(
         self.assertEqual(len(self.resources["aws_vpc"]), 1)
 
 
-class AWSProvisionStageTwoServiceServerlessAndTwoResourceDynamoDBTest(
+class AWSProvisionStageOneTwoServiceServerlessAndTwoResourceDynamoDBTest(
     AWSProvisionStageTestBase
 ):
     fixture_name = "serverless-2-service-2-dynamodb"
@@ -352,3 +352,21 @@ class AWSProvisionStageTwoServiceServerlessAndTwoResourceDynamoDBTest(
         self.assertEqual(len(self.resources["aws_dynamodb_table"]), 2)
         self.assertEqual(len(self.resources["aws_lambda_function"]), 2)
         self.assertEqual(len(self.resources["aws_vpc"]), 1)
+
+
+class AWSProvisionStageOneBackendOneServerlessOneDatabaseTest(
+    AWSProvisionStageTestBase
+):
+    fixture_name = "backend-1-serverless-1-service-1-db"
+    env_data_filepath = "environments_backend_1_serverless_1_service_1_db.yml"
+
+    def test_tf_is_valid(self):
+        self.assertEqual(len(self.resources["aws_lambda_function"]), 1)
+        self.assertEqual(len(self.resources["aws_db_instance"]), 1)
+        self.assertEqual(len(self.resources["aws_db_proxy"]), 1)
+        self.assertEqual(len(self.resources["aws_ecs_service"]), 1)
+        self.assertEqual(len(self.resources["aws_cloudfront_distribution"]), 1)
+        self.assertEqual(len(self.resources["aws_ecr_repository"]), 1)
+        self.assertEqual(len(self.resources["aws_vpc"]), 1)
+        self.assertEqual(len(self.resources["aws_acm_certificate"]), 2)
+        self.assertEqual(len(self.resources["aws_route53_zone"]), 1)
