abf4fabd8e6ffa5573311ccf579060cf5d423c94
Rafael Araujo <mendonca.rafael.araujo@gmail.com>
2024-08-16 12:53:54 -0300

    test: fix tg name substring

--- src/cnc/tests/test_provisioner.py
+++ src/cnc/tests/test_provisioner.py
@@ -428,5 +428,5 @@ class AWSProvisionStageOneBackendOneServerlessOneDatabaseTest(
         self.assertEqual(len(self.resources["aws_vpc"]), 1)
         self.assertEqual(len(self.resources["aws_acm_certificate"]), 2)
         self.assertEqual(len(self.resources["aws_route53_zone"]), 1)
-        self.assertEqual(len(self.resources["aws_security_group"]), 1)
+        self.assertEqual(len(self.resources["aws_security_group"]), 8)
         self.assertEqual(len(self.resources["aws_lb_target_group"]), 1)
