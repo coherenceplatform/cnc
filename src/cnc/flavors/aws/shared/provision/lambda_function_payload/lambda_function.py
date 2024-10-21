afae509ad7474246f2194b418da5ca8be5905927
Rafael Araujo <mendonca.rafael.araujo@gmail.com>
2024-10-21 17:20:23 -0300

    chore: lint

--- src/cnc/flavors/aws/shared/provision/lambda_function_payload/lambda_function.py
+++ src/cnc/flavors/aws/shared/provision/lambda_function_payload/lambda_function.py
@@ -1,2 +1,6 @@
 def lambda_handler(event, context):
-    return {"headers":{"Content-Type": "text/html"}, "statusCode": 200, "body": "Hello from Lambda!"}
+    return {
+        "headers": {"Content-Type": "text/html"},
+        "statusCode": 200,
+        "body": "Hello from Lambda!",
+    }
